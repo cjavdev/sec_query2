49d71e04f2337ab93746c93de342889ee92b4c2c
trevor <trevor@carrot.(none)>
2013-09-24 21:36:10 +0200

    add necessary depends

--- lib/sec_query.rb
+++ lib/sec_query.rb
@@ -1,9 +1,11 @@
 require "rubygems"
+require "i18n"
 require "rest-client"
 require "hpricot"
-
+require "active_support/core_ext"
 
 require "sec_query/version"
+require "sec_query/jsonable"
 require "sec_query/entity"
 require "sec_query/relationship"
 require "sec_query/transaction"
