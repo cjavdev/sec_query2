4223a52b52d2b49a06427c395eea40bae445443e
trevor <trevor@npolar.no>
2013-10-22 16:12:02 +0200

    handle nil case

--- lib/sec_query/transaction.rb
+++ lib/sec_query/transaction.rb
@@ -65,44 +65,47 @@ module SecQuery
             url = "http://www.sec.gov/cgi-bin/own-disp?action=get"+entity[:type]+"&CIK="+entity[:cik]+"&start="+start.to_s+"&count="+count.to_s
             response = Entity.query(url)
             doc = Hpricot(response)
-            trans = doc.search("//td[@width='40%']")[0].parent.parent.search("//tr")
-            i= start;
-            query_more = false;
-            for tran in trans
-                td = tran.search("//td")
-                if td[2] != nil and td[1].innerHTML != "Exercise"
-                        query_more = true;
-                        if !td[0].empty?
-                        transaction={}
-                        transaction[:symbol] = entity[:symbol]
-                        transaction[:code] = td[0].innerHTML;
-                        transaction[:date] = td[1].innerHTML;
-                        transaction[:reporting_owner] = td[2].innerHTML;
-                        transaction[:form] = td[3].innerHTML;
-                        transaction[:type] = td[4].innerHTML;
-                        transaction[:modes] = td[5].innerHTML;
-                        transaction[:shares] = td[6].innerHTML;
-                        transaction[:price] = td[7].innerHTML;
-                        transaction[:owned] = td[8].innerHTML;
-                        transaction[:number] = td[9].innerHTML;
-                        transaction[:owner_cik] = td[10].innerHTML;
-                        transaction[:security_name] = td[11].innerHTML;
-                        transaction[:deemed] = td[12].innerHTML;
-                        if trans[i+1]; n_td = trans[i+1].search("//td"); end
-                        if n_td != nil and n_td.count ==7 and n_td[0].innerHTML.empty?                         
-                            transaction[:exercise] = n_td[1].innerHTML;
-                            transaction[:nature] = n_td[2].innerHTML;
-                            transaction[:derivative] = n_td[3].innerHTML;
-                            transaction[:underlying_1] = n_td[4].innerHTML;
-                            transaction[:exercised] = n_td[5].innerHTML;
-                            transaction[:underlying_2] = n_td[6].innerHTML;
-                            transaction[:expires] = n_td[7].innerHTML;
-                            transaction[:underlying_3] =n_td[8].innerHTML;
-                        end
-                         entity[:transactions] << Transaction.new(transaction)
-                    end
-                end
-                i=i+1 
+            node = doc.search("//td[@width='40%']")[0]
+            if !node.nil?
+              trans = node.parent.parent.search("//tr")
+              i= start;
+              query_more = false;
+              for tran in trans
+                  td = tran.search("//td")
+                  if td[2] != nil and td[1].innerHTML != "Exercise"
+                          query_more = true;
+                          if !td[0].empty?
+                          transaction={}
+                          transaction[:symbol] = entity[:symbol]
+                          transaction[:code] = td[0].innerHTML;
+                          transaction[:date] = td[1].innerHTML;
+                          transaction[:reporting_owner] = td[2].innerHTML;
+                          transaction[:form] = td[3].innerHTML;
+                          transaction[:type] = td[4].innerHTML;
+                          transaction[:modes] = td[5].innerHTML;
+                          transaction[:shares] = td[6].innerHTML;
+                          transaction[:price] = td[7].innerHTML;
+                          transaction[:owned] = td[8].innerHTML;
+                          transaction[:number] = td[9].innerHTML;
+                          transaction[:owner_cik] = td[10].innerHTML;
+                          transaction[:security_name] = td[11].innerHTML;
+                          transaction[:deemed] = td[12].innerHTML;
+                          if trans[i+1]; n_td = trans[i+1].search("//td"); end
+                          if n_td != nil and n_td.count ==7 and n_td[0].innerHTML.empty?                         
+                              transaction[:exercise] = n_td[1].innerHTML;
+                              transaction[:nature] = n_td[2].innerHTML;
+                              transaction[:derivative] = n_td[3].innerHTML;
+                              transaction[:underlying_1] = n_td[4].innerHTML;
+                              transaction[:exercised] = n_td[5].innerHTML;
+                              transaction[:underlying_2] = n_td[6].innerHTML;
+                              transaction[:expires] = n_td[7].innerHTML;
+                              transaction[:underlying_3] =n_td[8].innerHTML;
+                          end
+                           entity[:transactions] << Transaction.new(transaction)
+                      end
+                  end
+                  i=i+1 
+              end
             end
             if query_more and limit == nil || query_more and !limit
                 Transaction.find(entity, start+count, count, limit);
